988639fad0a81c36153351ac8ff4f2954d8e5f5f
Ricky Sanchez <you@example.com>
2016-06-28 14:38:32 -0700

    initial commit

---
+++ src/recipes.js
@@ -0,0 +1,20 @@
+
+module.exports = {
+    "old-fashioned": "An old fashioned is created with 1 orange wedge, 1 sugar cube, dash of bitters, crushed ice, 2 shots bourbon, 2 tablespoons wholeberry cranberry sauce, club soda, garnish it with an orange twist.",
+    "tailgate sipper": "A tailgate sipper is created with 4 cups cubed pineapple, 3 shots bourbon, 1 cup chilled lemon sparking water, 1 tablespoon fresh lemon juice, 1 teaspoon vanilla extract.",
+    "fig and bourbon fizz": "A fig and bourbon fizz is created with 1 ripe fig, 6 fresh mint leaves, 2 to 3 brown sugar cubes, 2 shots bourbon, ice cubes, 1/2 cup chilled ginger ale, garnish with a mint sprig.",
+    "derby julep": "A derby julep can be created with 4 mint leaves, 2 tablespoons simple syrup, crushed ice, 2 shots bourbon, garnish with a fresh mint sprig.",
+    "mississippi bourbon punch": "A mississippi bourbon punch is created with 2 bottles dry muscadine wine, 1 bottle grenadine, 1 1/2 cups chilled bourbon, 1 cup fresh orange juice, 1 cup cranberry juice, 8 cups cups ice cubes, 12 oz can lemon soft drink, 1 cup chilled club soda, garnish with orange slices.",
+    "pecan milk punch": "A pecan milk punch is created with 1 cup chopped pecans, 1/2 cup cane syrup, 1 tablespoon cream of coconut, 1 teaspoon ground cinnamon, 1/2 teaspoon vanilla extract, 1/8 teaspoon kosher salt, 1 cup water, 2 shots bourbon, garnish with sweetened whipped cream, and fresh mint leaves.",
+    "shoo fly punch": "A shoo fly punch is created with 5 tablespoons bourbon, 2 tablespoons ginger liqueur, 1 tablespoon fresh lemon juice, 1 tablespoon simple syrup, 1/4 teaspoon bitters, crushed ice, nonalcoholic ginger beer, garnish with orange slices and fresh mint sprigs.",
+    "spiked arnold palmer": "A spiked arnold palmer is created with 5 cups boiling water, 5 regular tea bags, 3/4 sugar, 1 teaspoon lemon zest, 5 cups cold water, 1 cup bourbon, 1/2 cup fresh lemon juice, garnish with lemon slices.",
+    "bourbon sour": "A bourbon sour can be created with ice, 2 ounces bourbon, 3/4 ounce fresh squeezed lemon juice, 3/4 ounce simple syrup, 1 dash bitters.",
+    "cran bourbon and orange": "A cran bourbon and orange is created with 1 shot bourbon, 1 tablespoon cranberry reduction, 1 tablespoon orange juice, ice cubes, club soda, garnish with fresh rosemary.",
+    "iced mexican chocolate sipper": "A iced mexican chocolate sipper is created with 28 oz chocolate ice cream, 2 cups milk, 1 teaspoon cinnamon, 1/2 teaspoon orange zest, 1 cup bourbon, garnish with orange rind curls",
+    "sazerac": "A sazerac can be created with 1 cup sugar, 1 cup water, ice cubes, 1/4 cup bourbon, 1/4 teaspoon bitters, 1/4 teaspoon anise liqueur, garnish with a lemon rind twist.",
+    "manhattan": "A manhattan is created with 3 oz rye whiskey, 1 oz sweet vermouth, 3 dashes bitters, 1 maraschino cherry.",
+    "evening shade": "An evening shade is be created with 1/2 oz lime juice, 3 slices cucumber, 2 oz peach puree, 1 1/2 oz corn whiskey, 3/4 oz Domaine de Canton ginger Liqueur, 1/2 oz PAMA pomegranate liqueur, 2 sprigs lemon balm.",
+    "full windsor": "A full windsor is be created with 7oz Scotch, 7oz apple brandy, 5 1/4 oz Carpano Antica sweet vermouth, 1 3/4 oz Benedictine liqueur, 3 1/2 oz water, 1/4 oz bitters, 1/4 Peychauds bitters, garnish with an orange twist.",
+    "the libertine": "A libertine is created with 2 oz simple syrup, 2 sprigs fresh rosemary, 4 oz bourbon, 2 oz fresh lemon juice, 2 tablespoon orange marmalade, 1 tablespoon maple syrup, 1 tablespoon orange juice, 1 egg white.",
+    "doctor 7": "A doctor seven is created with 4 oz Dr Pepper, 2 oz Jack Daniels number 7 whiskey, and ice cubes."
+};
